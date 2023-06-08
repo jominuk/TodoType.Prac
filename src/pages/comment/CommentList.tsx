e8f5ffdf8c2221b79d224eb8f8e3873f8c3efce2
jominuk <jominuk1025@naver.com>
2023-06-08 17:34:31 +0900

    ✨ 수정버튼 클릭 시 input 변환

--- src/pages/comment/CommentList.tsx
+++ src/pages/comment/CommentList.tsx
@@ -1,5 +1,5 @@
 import { useQuery, useQueryClient } from "@tanstack/react-query";
-import React, { useCallback, useEffect } from "react";
+import React, { useCallback, useState } from "react";
 import { CommentApi } from "src/api/todo";
 import styled from "styled-components";
 import StButton from "src/components/button/Button";
@@ -8,6 +8,11 @@ import { useParams } from "react-router-dom";
 
 const CommentList = () => {
   const { id } = useParams();
+
+  const [editOn, setEditOn] = useState<string>("");
+  //el.id랑 비교해서 일치하는것 구분해서 input창으로 바꾸기 ==> 수정 버튼 클릭시 edtiOn 에 아이디들어옴
+  const [input, setInput] = useState<string>("");
+  // 수정완료시 input창에 작성한 값 받아오기
 
   const queryClient = useQueryClient();
 
@@ -19,22 +24,24 @@ const CommentList = () => {
     queryClient.invalidateQueries({ queryKey: ["comments"] });
   }, []);
 
+  const onEditComplete = useCallback(() => {}, []);
+
   return (
     <StCommentContainer>
       {Array.isArray(data) &&
         data.map((el) => {
-          return (
+          return el.id !== editOn ? (
             <StComment key={`comment_${el.id}`}>
               <StCalendar>{el.date}</StCalendar>
               <div>{el.comment}</div>
 
               <StButtonGroup>
                 <StButton
-                  //아이디 에딧온으로 바꾸고 input이라는 스테이트에 코멘트값 넣기
-                  //   onClick={() => {
-                  //     setEditOn(el.id);
-                  //     setInput(el.comment);
-                  //   }}
+                  // 아이디 에딧온으로 바꾸고 input이라는 스테이트에 코멘트값 넣기
+                  onClick={() => {
+                    setEditOn(el.id);
+                    setInput(el.comment);
+                  }}
                   borderColor="teal"
                   width="50px"
                   height="30px"
@@ -48,6 +55,37 @@ const CommentList = () => {
                   onClick={() => onDeleteComment(el.id)}
                 >
                   삭제
+                </StButton>
+              </StButtonGroup>
+            </StComment>
+          ) : (
+            <StComment key={`comment_${el.id}`}>
+              <StCalendar>{el.date}</StCalendar>
+              <Stinput
+                onChange={(e) => setInput(e.target.value)}
+                value={input}
+              />
+
+              <StButtonGroup>
+                <StButton
+                  onClick={
+                    () => onEditComplete()
+                    // el.id
+                  }
+                  borderColor="teal"
+                  width="50px"
+                  height="30px"
+                >
+                  완료
+                </StButton>
+                <StButton
+                  borderColor="red"
+                  width="50px"
+                  height="30px"
+                  //에딧온 바꿔줘서 일치하는 아이디 없게 만들기
+                  onClick={() => setEditOn("")}
+                >
+                  취소
                 </StButton>
               </StButtonGroup>
             </StComment>
